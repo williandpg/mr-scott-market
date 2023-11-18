bc26b6766ab4192c629caa51465bc66d24775f72
Willian Gonçalves <willian.p.g@outlook.com>
2023-11-18 11:55:46 -0300

    Requisito 04 - Crie a função updateItem

--- src/functions.ts
+++ src/functions.ts
@@ -1,3 +1,8 @@
 export function first<T>(items: T[]): T {
   return items[0];
 }
+
+export function updateItem<T>(item: T[], index: number, updatedItem: T): T[] {
+  if (index >= 0 && index < item.length) { item.splice(index, 1, updatedItem); }
+  return item;
+}
