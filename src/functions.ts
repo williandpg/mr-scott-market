71ca18d20449b1943f02e1eb26213cdaf47a0fba
Willian Gonçalves <willian.p.g@outlook.com>
2023-11-18 12:09:16 -0300

    Requisito 06 - Crie a função buildCartItem

--- src/functions.ts
+++ src/functions.ts
@@ -1,3 +1,7 @@
+import { Food } from './types/Food';
+import { Drink } from './types/Drink';
+import { CartItem } from './types/CartItem';
+
 export function first<T>(items: T[]): T {
   return items[0];
 }
@@ -5,4 +9,12 @@ export function first<T>(items: T[]): T {
 export function updateItem<T>(item: T[], index: number, updatedItem: T): T[] {
   if (index >= 0 && index < item.length) { item.splice(index, 1, updatedItem); }
   return item;
+}
+
+export function buildCartItem({ name, price }: Food | Drink, quantity: number): CartItem {
+  return {
+    name,
+    price,
+    quantity,
+  } as CartItem;
 }
